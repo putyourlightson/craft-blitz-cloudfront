fe7ea3fbefc69316f1310d5be4a614c4df84ba9c
Ben Croker <ben@putyourlightson.com>
2024-02-28 16:48:13 -0600

    Better test

--- tests/pest/Feature/CondenseUrlsTest.php
+++ tests/pest/Feature/CondenseUrlsTest.php
@@ -14,11 +14,12 @@ beforeEach(function() {
 test('URLs are condensed into a single URL with a wildcard character after the longest common prefix', function() {
     $purger = new CloudFrontPurger();
     $urls = [
-        'https://example.com/foo/bar/baz/qux',
         'https://example.com/foo',
+        'https://example.com/foo/bar',
         'https://example.com/foo/bar/baz',
+        'https://example.com/fun',
     ];
 
     expect($purger->getCondensedUrls($urls))
-        ->toBe(['https://example.com/foo*']);
+        ->toBe(['https://example.com/f*']);
 });
